a33590339ac18307e320be0b01ab0d5c75aaea14
Ivan Barrios <ivan.barrios@tetakawi.com>
2026-02-13 15:03:39 -0600

    improve icon password

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { onMounted, ref, computed } from 'vue';
+import { EyeIcon, EyeSlashIcon } from '@heroicons/vue/24/outline';
 
 const model = defineModel({
     type: String,
@@ -46,44 +47,16 @@ defineExpose({ focus: () => input.value?.focus() });
             v-if="props.type === 'password'"
             type="button"
             @click="showPassword = !showPassword"
-            class="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-400 hover:text-gray-600"
+            class="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-400 hover:text-[#0071ce] transition-colors duration-200"
         >
-            <!-- Ojo cerrado -->
-            <svg
+            <EyeIcon
                 v-if="!showPassword"
-                xmlns="http://www.w3.org/2000/svg"
                 class="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-            >
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M15 12m-3 0a3 3 0 106 0a3 3 0 10-6 0M2.458 12C3.732 7.943 7.523 5 12 5
-                    c4.477 0 8.268 2.943 9.542 7
-                    -1.274 4.057-5.065 7-9.542 7
-                    -4.477 0-8.268-2.943-9.542-7z" />
-            </svg>
-
-            <!-- Ojo abierto -->
-            <svg
+            />
+            <EyeSlashIcon
                 v-else
-                xmlns="http://www.w3.org/2000/svg"
                 class="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-            >
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M13.875 18.825A10.05 10.05 0 0112 19
-                    c-4.478 0-8.268-2.943-9.542-7
-                    a9.956 9.956 0 012.293-3.95
-                    M6.423 6.423A9.956 9.956 0 0112 5
-                    c4.477 0 8.268 2.943 9.542 7
-                    a9.966 9.966 0 01-4.043 5.18
-                    M15 12a3 3 0 00-3-3
-                    m0 0a3 3 0 00-3 3
-                    m3-3l6 6m-6-6l-6 6" />
-            </svg>
+            />
         </button>
     </div>
 </template>
